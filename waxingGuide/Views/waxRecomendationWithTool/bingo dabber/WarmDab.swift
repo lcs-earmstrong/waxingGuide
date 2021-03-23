75abe35bb825ff18ca62d84b7acfde090299b658
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-23 07:09:27 -0400

    Done all of the liquid wax views

--- waxingGuide/Views/waxRecomendationWithTool/bingo dabber/WarmDab.swift
+++ waxingGuide/Views/waxRecomendationWithTool/bingo dabber/WarmDab.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct WarmDab: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+        Text("For warmer conditions like these you really need to make sure that you let the wax soak in because the wet snow really sucks the wax out of your skis.  For these temperatures the best wax is the HS8 liquid.")
+            Image("warmLiquid")
         Text("Apply the wax by 'dabbing' some out and then rubbing it in.  Give it time to cool/soak in and than go ski.")
+        }
     }
 }
 
--- waxingGuide/Views/waxRecomendationWithTool/bingo dabber/warmestDab.swift
+++ waxingGuide/Views/waxRecomendationWithTool/bingo dabber/warmestDab.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct warmestDab: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+        Text("These conditions are never really fast and really suck all of the wax out of your skis so if you are going for a long ski I would definitely recommend using hard wax and a tool but in a pinch the best liquid wax is the HS10 liquid.")
+            Image("warmestLiquid")
         Text("Apply the wax by 'dabbing' some out and then rubbing it in.  Give it time to cool/soak in and than go ski.")
+        }
     }
 }
 
